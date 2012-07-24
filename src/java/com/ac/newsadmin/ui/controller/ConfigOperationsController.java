5d5cf0b007ca4c6154f4d1cf395aa66d3a2406d2
acemrek <acemrek@gmail.com>
2012-07-24 23:21:11 +0300

    config gelistirmeler

--- src/java/com/ac/newsadmin/ui/controller/ConfigOperationsController.java
+++ src/java/com/ac/newsadmin/ui/controller/ConfigOperationsController.java
@@ -5,18 +5,48 @@ import java.io.Serializable;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import com.ac.newsadmin.ui.tools.configTools.PhotoSizeConfigTool;
+
 @Component("configOperationsBean")
 @Scope("view")
 public class ConfigOperationsController extends AbstractOperationController implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 
-	private static final String CONFIG_OPERATION_CHANGE_PREFERENCES = "changePreferences";
+	private static final String WRITTEN_CONTENT_PHOTO_SIZES = "writtenContentPhotoSizes";
+	private static final String COLUMNIST_PHOTO_SIZES = "columnistPhotoSizes";
+	private static final String PHOTO_GALERY_PHOTO_SIZES = "photoGaleryPhotoSizes";
+	private static final String VIDEO_THUMBNAIL_PHOTO_SIZES = "videoThumbnailPhotoSizes";
 	
-	public void showChangePreferencesForm(){
-		setSelectedOperation(CONFIG_OPERATION_CHANGE_PREFERENCES);
+	private PhotoSizeConfigTool writtenContentPhotoSizeTool;
+	
+	public void showWrittenContentPhotoSizesForm(){
+		if(writtenContentPhotoSizeTool == null){
+			writtenContentPhotoSizeTool = new PhotoSizeConfigTool();
+		}
+		setSelectedOperation(WRITTEN_CONTENT_PHOTO_SIZES);
+	}
+
+	public void showColumnistPhotoSizesForm(){
+		setSelectedOperation(COLUMNIST_PHOTO_SIZES);
 	}
 	
+	public void showPhotoGaleryPhotoSizesForm(){
+		setSelectedOperation(PHOTO_GALERY_PHOTO_SIZES);
+	}
+	
+	public void showVideoThumbnailPhotoSizesForm(){
+		setSelectedOperation(VIDEO_THUMBNAIL_PHOTO_SIZES);
+	}
+	
+	public PhotoSizeConfigTool getWrittenContentPhotoSizeTool() {
+		return writtenContentPhotoSizeTool;
+	}
+
+	public void setWrittenContentPhotoSizeTool(PhotoSizeConfigTool writtenContentPhotoSizeTool) {
+		this.writtenContentPhotoSizeTool = writtenContentPhotoSizeTool;
+	}
+
 	@Override
 	public String getDiscriminator() {
 		return "configOperations";
